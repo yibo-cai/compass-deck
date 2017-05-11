c1f7fe76022e082a6cac533cde33d1ed350a8156
huangxiangyu <huangxiangyu5@huawei.com>
2017-05-11 19:41:26 +0800

    add deployment

--- start.sh
+++ start.sh
@@ -4,6 +4,5 @@ until mysql -h"compass-db" -u"root" -p"root" -e 'show databases'; do
   sleep 1
 done
 >&2 echo "DB is up"
-/opt/compass/bin/manage_db.py createdb
 apachectl -k start
 tail -f /dev/null
